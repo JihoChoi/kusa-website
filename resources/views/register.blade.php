0ad2e7d39fdcffa2f19d8cde462de8300b206a37
Dongyu Kang <dongyu@dongyukang.com>
2016-12-08 02:57:31 -0500

    some fix registration validation

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -34,35 +34,35 @@
         <div class = "form-group">
           <label for="firstname" class = "col-sm-3 control-label">Firstname</label>
           <div class = "col-sm-9">
-            <input id = "firstname" type = "text" class = "form-control" name = "firstname" placeholder = "First name*" required = "true">
+            <input id = "firstname" type = "text" class = "form-control" name = "firstname" placeholder = "First name*" required = "true" data-error = "Please enter your first name">
             <div class = "help-block with-errors"></div>
           </div>
         </div>
         <div class = "form-group">
           <label for="lastname" class = "col-sm-3 control-label">Lastname</label>
           <div class = "col-sm-9">
-            <input id = "lastname" type = "text" class = "form-control" name = "lastname" placeholder = "Last name*" required = "true">
+            <input id = "lastname" type = "text" class = "form-control" name = "lastname" placeholder = "Last name*" required = "true" data-error = "Please enter your last name">
             <div class = "help-block with-errors"></div>
           </div>
         </div>
         <div class = "form-group">
           <label for = "email" class = "col-sm-3 control-label">Email</label>
           <div class = "col-sm-9">
-            <input id = "email" type = "email" class = "form-control" name = "email" placeholder = "Email address*" data-error = "valid email address is required" required = "true">
+            <input id = "email" type = "email" class = "form-control" name = "email" placeholder = "Email address*" data-error = "Please enter your valid email address" required = "true">
             <div class = "help-block with-errors"></div>
           </div>
         </div>
         <div class = "form-group">
           <label for = "register-password" class = "col-sm-3 control-label">Password</label>
           <div class = "col-sm-9">
-            <input id = "register-password" type = "password" class = "form-control" name = "password" placeholder = "Password*" required = "true">
+            <input id = "register-password" type = "password" class = "form-control" name = "password" placeholder = "Password*" required = "true" data-error = "A password is required">
             <div class = "help-block with-errors"></div>
           </div>
         </div>
         <div class = "form-group">
           <label for = "register-password-reconfirm" class = "col-sm-3 control-label">Confirm Password</label>
           <div class = "col-sm-9">
-            <input id = "register-password-reconfirm" type = "password" class = "form-control" name = "password-reconfirm" placeholder = "Confirm Password*" required = "true" data-match = "#register-password" data-match-error = "Passwords don't match!">
+            <input id = "register-password-reconfirm" type = "password" class = "form-control" name = "password-reconfirm" placeholder = "Confirm Password*" required = "true" data-error = "A password is required" data-match = "#register-password" data-match-error = "Password does not match">
             <div class = "help-block with-errors"></div>
           </div>
         </div>
@@ -87,7 +87,7 @@
         <div class = "form-group">
           <label for="phone" class = "col-sm-3 control-label">Phone Number</label>
           <div class = "col-sm-9">
-            <input id="phone" type = "text" class = "form-control" name = "phone" placeholder = "US Phone Number">
+            <input id="phone" type = "text" class = "form-control" name = "phone" placeholder = "US Phone Number (not required)">
           </div>
         </div>
         <div class = "form-group" style = "margin-top: 10px">
