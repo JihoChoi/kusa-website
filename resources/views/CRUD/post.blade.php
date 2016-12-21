a6875db581ec0ed5f40e38783096a4b8af900e28
Dongyu Kang <dongyu@dongyukang.com>
2016-12-21 02:32:18 -0500

    added multiple file uploaders, but cannot handle the uploads yet

--- resources/views/CRUD/post.blade.php
+++ resources/views/CRUD/post.blade.php
@@ -27,13 +27,20 @@
       </div>
     </div>
     <div class = "container">
+      <form enctype = "multipart/form-data" action = "profile" method = "POST" style = "margin-left: 150px;">
+        {{ csrf_field() }}
+        <input type = "file" name = "profile" class = "pull-left" style = "margin-top: 5px;" multiple>
+        <button type = "submit" class = "pull-right btn btn-primary"><i class = "fa fa-cloud-upload"></i> Upload</button>
+      </form>
+    </div>
+    <div class = "container">
       <div class = "form-group">
         <textarea class = "form-control" name = "content_area"></textarea>
       </div>
     </div>
     <div class = "container">
       <div class = "form-group">
-        <button type = "submit" class = "btn btn-primary btn-lg">Post</button>
+        <button type = "submit" class = "btn btn-primary pull-right"><i class = "fa fa-plus"></i> Post</button>
       </div>
     </div>
   </form>
