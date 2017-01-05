cf4dfd2d598b6946bec243d627ff1afaffe27976
Dongyu Kang <dongyu@dongyukang.com>
2017-01-04 21:48:21 -0500

    display board according to teams

--- resources/views/CRUD/USERS/board.blade.php
+++ resources/views/CRUD/USERS/board.blade.php
@@ -20,17 +20,18 @@ body {
     <div class = "page-header"><h2>{{ $team->team_name }}</h2></div>
     <div class = "row">
     @foreach ($active_members as $member)
-      @if ($member->kusa_team == $team->team_name)
-        <div class = "col-sm-5 col-md-3">
-          <div class = "thumbnail">
-            <img src = "/images/profiles/{{ $member->profile_img }}" class = "img-responsive" width = "350px" height = "350px">
-            <div class = "caption" style = "text-align: center;">
-              <h3>{{ $member->lastname }} {{ $member->firstname }}</h3>
-              <p> {{ $member->kusa_role }} </p>
+      @foreach ($member->teams as $team_member)
+        @if ($team_member->team_name == $team->team_name)
+          <div class = "col-sm-5 col-md-3">
+            <div class = "thumbnail">
+              <img src = "/images/profiles/{{ $member->profile_img }}" class = "img-responsive" width = "350px" height = "350px">
+              <div class = "caption" style = "text-align: center;">
+                <h3>{{ $member->lastname }} {{ $member->firstname }}</h3>
+              </div>
             </div>
           </div>
-        </div>
-      @endif
+        @endif
+      @endforeach
     @endforeach
   </div>
   @endforeach
