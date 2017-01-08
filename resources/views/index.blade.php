d194673312c92f8313c77222349b57defcc4b5b7
Dongyu Kang <dongyu@dongyukang.com>
2017-01-07 23:17:05 -0500

    post display front-end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,16 +9,15 @@
     </div>
     <div class = "container sub-video">
       <div class = "outer">
-        <video id = "intro" controls = "controls" width="560" height="315" allowfullscreen>
-          <source src = "{{ asset('videos/kusa_video.mp4') }}"/>
-          <source src = "{{ asset('videos/kusa_video.webm') }}" type = "video/webm"/>
-          <source src = "{{ asset('videos/kusa_video.ogv') }}" type = "video/ogv"/>
-          <source src = "{{ asset('videos/kusa_video.m4v') }}" type = "video/mp4"/>
+        <video id = "intro" preload controls allowfullscreen>
+          <source src = "videos/kusa_video.mp4" type = 'video/mp4;'/>
+          <source src = "videos/kusa_video.webm" type = 'video/webm;'/>
+          <source src = "videos/kusa_video.ogv" type = 'video/ogv;'/>
+          <source src = "videos/kusa_video.m4v" type = 'video/mp4;'/>
           Your browser does not support this video.
         </video>
       </div>
     </div>
-
     <div class = "container" style = "background-color: #fafafa; width: 100vw; margin-left: -50vw; left: 50%;">
       <div class = "container" style = "position: relative;">
         <div class = "page-header">
@@ -28,20 +27,20 @@
         <div class = "row" style = "margin-top: 30px;">
           <div class = "col-sm-2 col-md-4 col-lg-4">
             <div class = "thumbnail">
-              <!--<img src = "/images/events/post.jpg" class = "img-responsive" width = "550px">-->
+            <img src = "/images/events/post.jpg" class = "img-responsive" width = "550px">
               <div class = "caption" >
                 <center><h4>PEPERO DAY</h4></center>
                 <hr class = "style5">
                 <p>
                   아직도 꿈 속의 낭군님을 쳐다보고만 계신가요? 뭐라구요? 공대 여신님께 사귀자는 말을 못하겠다구요?걱정마세요!
-                  그래서 퍼듀 쿠사가 준비했습니다! ... 
+                  그래서 퍼듀 쿠사가 준비했습니다! ...
               </p>
               </div>
             </div>
           </div>
           <div class = "col-sm-2 col-md-4 col-lg-4">
             <div class = "thumbnail">
-              <!--<img src = "/images/events/kusa_all.jpg" class = "img-responsive" width = "550px">-->
+              <img src = "/images/nightsky.jpg" class = "img-responsive" width = "550px">
               <div class = "caption">
                 <center><h4>D-Korea</h4></center>
                 <hr class = "style5">
@@ -54,7 +53,7 @@
           </div>
           <div class = "col-sm-2 col-md-4 col-lg-4">
             <div class = "thumbnail">
-              <!--<img src = "/images/events/kusa_all.jpg" class = "img-responsive" width = "550px">-->
+              <img src = "/images/kusa_radio.jpg" class = "img-responsive" width = "550px">
               <div class = "caption">
                 <center><h4>Volunteer</h4></center>
                 <hr class = "style5">
@@ -75,8 +74,8 @@
           <h4 style = "color: #f4645f; font-family: 'Ubuntu', sans-serif; letter-spacing: .2em;">Events</h4>
         </div>
         <div class = "row">
-          <div class = "col-md-7 event-box">
-            <center><img src = "/images/111.jpg" class = "img-responsive"></center>
+          <div class = "col-md-6 event-box">
+            <center><img src = "/images/kusa_radio.jpg" class = "img-responsive"></center>
             <div class = "thumbnail">
               <div class = "caption">
                 <p class = "category">News</p>
@@ -84,8 +83,8 @@
               </div>
             </div>
           </div>
-          <div class = "col-md-5 event-box">
-            <img src = "/images/events/post.jpg" class = "img-responsive">
+          <div class = "col-md-6 event-box">
+            <img src = "/images/purdue2.jpg" class = "img-responsive">
             <div class = "thumbnail">
               <div class = "caption">
                 <p class = "category">Announcement</p>
@@ -96,7 +95,7 @@
         </div>
         <div class = "row">
           <div class = "col-md-4 event-box">
-            <img src = "/images/events/about.jpg" class = "img-responsive">
+            <img src = "/images/kusa_radio.jpg" class = "img-responsive">
             <div class = "thumbnail">
               <div class = "caption">
                 <p class = "category">D-Korea</p>
