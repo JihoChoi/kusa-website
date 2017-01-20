1f231c016cf2773ba62cbef49980ec70e5d994b7
Dongyu Kang <dongyu@dongyukang.com>
2017-01-19 23:27:55 -0500

    event-section divided two

--- resources/views/posts/events.blade.php
+++ resources/views/posts/events.blade.php
@@ -1,16 +1,44 @@
 @extends('layouts.index-layout')
 @section('main-content')
-<div class = "container" style = "margin-top: 100px;">
-  @foreach ($events as $event)
-    <div class = "container-fluid content-container">
-      <img class = "img-responsive" src = "/images/dispimg/{{ $event->dispimg }}">
-      <div class = "event-box">
-        <div class = "thumbnail">
-          <h4 class = "caption" style = "padding-left: 40px; font-family: 'Open Sans', sans-serif; color: white;">{{ $event->content_title }}</caption>
+<div class = "container" style = "">
+  <div class = "container event-image" style = "width: 100vw; margin-left: -50vw; left: 50%; background-color: #fafafa;">
+
+  </div>
+  <div class = "col-md-8">
+    @foreach ($events as $event)
+      <div class = "content-container">
+        <img class = "img-responsive" src = "/images/dispimg/{{ $event->dispimg }}">
+        <div class = "event-box">
+          <div class = "thumbnail">
+            <h4 class = "category">{{ date('F d, Y', strtotime($event->created_at)) }}</h4>
+            <p class = "category">{{ $event->event_category }}</p>
+            <h3 class = "caption" style = "padding-left: 40px; font-family: 'Open Sans', sans-serif; color: white;">{{ $event->content_title }}</h3>
+            <div class = "summary">
+              <?php
+                $content = strip_tags($event->content);
+                $content = mb_substr($content, 0, 200, "utf-8");
+                echo ($content);
+              ?>
+              ... (Read more)
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  @endforeach
+    @endforeach
   <center> {{ $events->render() }} </center>
+  </div>
+  <div class = "col-md-4">
+    <div class = "sidebox">
+      <h4 class = "sidebox-title">Trending</h4>
+
+    </div>
+  </div>
+  <div class = "col-md-4">
+    <div class = "sidebox">
+      <h4 class = "sidebox-title">Trending</h4>
+
+    </div>
+  </div>
 </div>
+@include('footer')
 @stop
